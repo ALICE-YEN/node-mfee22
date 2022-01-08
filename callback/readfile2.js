12de09cf9a1602ac8fa8be89aa070d41ffd129cb
Alice Yen <gp40424@gmail.com>
2022-01-09 03:59:06 +0800

    add readfile2

---
+++ callback/readfile2.js
@@ -0,0 +1,27 @@
+// Promise版
+const { readFile } = require("fs");
+
+// readFile非同步函式，丟給外包
+let readfilePromise = new Promise((resolve, reject) => {
+  readFile("test.txt", "utf-8", (err, data) => {
+      if (err) {
+          reject(err);
+          return;
+      }
+      resolve(data);
+  });
+});
+
+console.log(readfilePromise); // pending，因為剛剛包出來promise，還沒執行非同步readFile。
+
+readfilePromise
+.then((result) => {
+  // 這邊會接住resolve
+    console.log(`這裡是 Promise 的 result: ${result}`);
+  })  
+  .catch((err) => {
+    console.error("這裡是 Promise 的 catch:", err);
+  });
+
+
+// 複習2022.1.8
