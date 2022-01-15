8138ffe27831c8f201e4793a969090367ab2d5f1
Alice Yen <gp40424@gmail.com>
2022-01-15 12:02:11 +0800

    update crawler3

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -1,23 +1,13 @@
 // async/await axios.get 證交所資料
 
 const axios = require("axios");
-const { readFile } = require("fs/promises");
+const { readFile } = require("fs/promises"); //讀檔函示
 
 (async () => {
   try {
     // 從stock.txt撈stockNo
-    (async() => {
-        try {
-            let result = await readFile("stock.txt", "utf-8");
-            console.log(`${result}`);
-        } catch (err) {
-            console.log(err);
-        }
-    })();
-    
-
     // 根據變數去抓取資料
-    let stockNo = 2330;
+    let stockNo = await readFile("stock.txt", "utf-8");
     let queryDate = "20220114";
 
     let response = await axios.get(
